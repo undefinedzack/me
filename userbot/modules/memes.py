946c75e46a7c9e0537625109f977094be6cd6a1f
Avinash Reddy <cassilasreddythemostwanted3108@gmail.com>
2019-09-21 13:07:04 +0530

    CLEANUP - getting user object from event

--- userbot/modules/memes.py
+++ userbot/modules/memes.py
@@ -22,6 +22,7 @@ from cowpy import cow
 
 from userbot import CMD_HELP
 from userbot.events import register, errors_handler
+from userbot.modules.admin import get_user_from_event
 
 # ================= CONSTANT =================
 METOOSTR = [
@@ -673,7 +674,7 @@ async def coin(event):
 async def who(event):
     """ slaps a user, or get slapped if not a reply. """
 
-    replied_user = await get_user(event)
+    replied_user = await get_user_from_event(event)
     caption = await slap(replied_user, event)
     message_id_to_reply = event.message.reply_to_msg_id
 
@@ -687,42 +688,6 @@ async def who(event):
         await event.edit(
             "`Can't slap this person, need to fetch some sticks and stones !!`"
         )
-
-
-async def get_user(event):
-    """ Get the user from argument or replied message. """
-    if event.reply_to_msg_id:
-        previous_message = await event.get_reply_message()
-        replied_user = await event.client(
-            GetFullUserRequest(previous_message.from_id))
-    else:
-        user = event.pattern_match.group(1)
-
-        if user.isnumeric():
-            user = int(user)
-
-        if not user:
-            self_user = await event.client.get_me()
-            user = self_user.id
-
-        if event.message.entities is not None:
-            probable_user_mention_entity = event.message.entities[0]
-
-            if isinstance(probable_user_mention_entity,
-                          MessageEntityMentionName):
-                user_id = probable_user_mention_entity.user_id
-                replied_user = await event.client(GetFullUserRequest(user_id))
-                return replied_user
-        try:
-            user_object = await event.client.get_entity(user)
-            replied_user = await event.client(
-                GetFullUserRequest(user_object.id))
-
-        except (TypeError, ValueError):
-            await event.edit("`I don't slap aliens, they ugly AF !!`")
-            return None
-
-    return replied_user
 
 
 async def slap(replied_user, event):
--- userbot/modules/whois.py
+++ userbot/modules/whois.py
@@ -15,6 +15,7 @@ from telethon.tl.types import MessageEntityMentionName
 from telethon.utils import get_input_location
 from userbot import CMD_HELP, TEMP_DOWNLOAD_DIRECTORY
 from userbot.events import register, errors_handler
+from userbot.modules.admin import get_user_from_event
 
 
 @register(pattern=".whois(?: |$)(.*)", outgoing=True)
@@ -27,7 +28,7 @@ async def who(event):
     if not os.path.isdir(TEMP_DOWNLOAD_DIRECTORY):
         os.makedirs(TEMP_DOWNLOAD_DIRECTORY)
 
-    replied_user = await get_user(event)
+    replied_user = await get_user_from_event(event)
     photo, caption = await fetch_info(replied_user, event)
     message_id_to_reply = event.message.reply_to_msg_id
     if not message_id_to_reply:
@@ -45,36 +46,6 @@ async def who(event):
         await event.delete()
     except TypeError:
         await event.edit(caption, parse_mode="html")
-
-
-async def get_user(event):
-    """ Get the user from argument or replied message. """
-    if event.reply_to_msg_id:
-        previous_message = await event.get_reply_message()
-        replied_user = await event.client(
-            GetFullUserRequest(previous_message.from_id))
-    else:
-        user = event.pattern_match.group(1)
-        if user.isnumeric():
-            user = int(user)
-        if not user:
-            self_user = await event.client.get_me()
-            user = self_user.id
-        if event.message.entities is not None:
-            probable_user_mention_entity = event.message.entities[0]
-            if isinstance(probable_user_mention_entity,
-                          MessageEntityMentionName):
-                user_id = probable_user_mention_entity.user_id
-                replied_user = await event.client(GetFullUserRequest(user_id))
-                return replied_user
-        try:
-            user_object = await event.client.get_entity(user)
-            replied_user = await event.client(
-                GetFullUserRequest(user_object.id))
-        except (TypeError, ValueError) as err:
-            await event.edit(str(err))
-            return None
-    return replied_user
 
 
 async def fetch_info(replied_user, event):
